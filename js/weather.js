f7814b944a7e32537b16de56f8301c346509dbf9
Ram Zallan <ram.zallan@ht-la.org>
2016-06-12 19:19:16 -0700

    Added localStorage for custom locations, weather defaults to saved locations if they exist.

--- js/weather.js
+++ js/weather.js
@@ -10,74 +10,161 @@ $(document).ready(function () {
     'America/Los_Angeles|PST PDT|80 70|0101|1Lzm0 1zb0 Op0',
     'America/New_York|EST EDT|50 40|0101|1Lz50 1zb0 Op0'
 ]);
+    /* If user does not have local storage of locations set */
+    if (localStorage.getItem("weather-location1") === null &&
+        localStorage.getItem("weather-location2") === null) {
 
-    /* Defaults weather to LA */
-    $.simpleWeather({
-        location: 'Los Angeles, CA',
-        woeid: '',
-        unit: 'f',
-        success: function (weather) {
-            /* Weather icon, with alt of current weather */
-            html = '<img class="icon" src="images/icons/' + weather.code + '.svg" alt=' + weather.currently.replace(/\s+/g, '_') + '>';
-            /* Temperature */
-            html += '<h2 class="temp">' + weather.temp + '&deg;</h2>';
-            /* City */
-            html += '<h3 class="city">' + weather.city.toUpperCase() + '</h3>';
-            /* Date & Time */
-            html += '<p class="date">' + moment().format('dddd[,] MMMM Do').toUpperCase() + '<br>' + moment().tz("America/Los_Angeles").format('h:mm a') + '</p>'; + '</p>';
-            /* Current conditions */
-            html += '<p class="rn">' + weather.currently + '<p>';
-            /* High & low */
-            html += '<ul class="extremes"><li>' + weather.high + '</li><li>' + weather.low + '</li></ul>'
-                /* Forecasts */
+        /* Defaults weather to LA */
+        $.simpleWeather({
+            location: 'Los Angeles, CA',
+            woeid: '',
+            unit: 'f',
+            success: function (weather) {
+                /* Weather icon, with alt of current weather */
+                html = '<img class="icon" src="images/icons/' + weather.code + '.svg" alt=' + weather.currently.replace(/\s+/g, '_') + '>';
+                /* Temperature */
+                html += '<h2 class="temp">' + weather.temp + '&deg;</h2>';
+                /* City */
+                html += '<h3 class="city">' + weather.city.toUpperCase() + '</h3>';
+                /* Date & Time */
+                html += '<p class="date">' + moment().format('dddd[,] MMMM Do').toUpperCase() + '<br>' + moment().tz("America/Los_Angeles").format('h:mm a') + '</p>'; + '</p>';
+                /* Current conditions */
+                html += '<p class="rn">' + weather.currently + '<p>';
+                /* High & low */
+                html += '<ul class="extremes"><li>' + weather.high + '</li><li>' + weather.low + '</li></ul>'
+                    /* Forecasts */
+                    // Placeholder
+                html += '<div class="row"><div class="col s3"> </div>';
+                for (var i = 1; i < 4; i++) {
+                    html += '<div class="col s2 forecast"><img class="thumb" src="images/icons/' + weather.forecast[i].code + '.svg"><span><p>' + weather.forecast[i].day + '</p><ul class="extremes small"><li>' + weather.forecast[i].high + '</li><li>' + weather.forecast[i].low + '</li></ul></span></div>';
+                }
                 // Placeholder
-            html += '<div class="row"><div class="col s3"> </div>';
-            for (var i = 1; i < 4; i++) {
-                html += '<div class="col s2 forecast"><img class="thumb" src="images/icons/' + weather.forecast[i].code + '.svg"><span><p>' + weather.forecast[i].day + '</p><ul class="extremes small"><li>' + weather.forecast[i].high + '</li><li>' + weather.forecast[i].low + '</li></ul></span></div>';
+                html += '<div class="col s3"></div> </div>';
+
+                $("#la").html(html);
+            },
+            error: function (error) {
+                $("#la").html('<p>' + error + '</p>');
             }
-            // Placeholder
-            html += '<div class="col s3"></div> </div>';
+        });
 
-            $("#la").html(html);
-        },
-        error: function (error) {
-            $("#la").html('<p>' + error + '</p>');
-        }
-    });
-
-    $.simpleWeather({
-        location: 'Rochester, NY',
-        woeid: '',
-        unit: 'f',
-        success: function (weather) {
-            /* Weather icon, with alt of current weather */
-            html = '<img class="icon" src="images/icons/' + weather.code + '.svg" alt=' + weather.currently.replace(/\s+/g, '_') + '>';
-            /* Temperature */
-            html += '<h2 class="temp">' + weather.temp + '&deg;</h2>';
-            /* City */
-            html += '<h3 class="city">' + weather.city.toUpperCase() + '</h3>';
-            /* Date */
-            html += '<p class="date">' + moment().format('dddd[,] MMMM Do').toUpperCase() + '<br>' + moment().tz("America/New_York").format('h:mm a') + '</p>'; + '</p>';
-            /* Current conditions */
-            html += '<p class="rn">' + weather.currently + '<p>';
-            /* High & low */
-            html += '<ul class="extremes"><li>' + weather.high + '</li><li>' + weather.low + '</li></ul>'
-                /* Forecasts */
+        $.simpleWeather({
+            location: 'Rochester, NY',
+            woeid: '',
+            unit: 'f',
+            success: function (weather) {
+                /* Weather icon, with alt of current weather */
+                html = '<img class="icon" src="images/icons/' + weather.code + '.svg" alt=' + weather.currently.replace(/\s+/g, '_') + '>';
+                /* Temperature */
+                html += '<h2 class="temp">' + weather.temp + '&deg;</h2>';
+                /* City */
+                html += '<h3 class="city">' + weather.city.toUpperCase() + '</h3>';
+                /* Date */
+                html += '<p class="date">' + moment().format('dddd[,] MMMM Do').toUpperCase() + '<br>' + moment().tz("America/New_York").format('h:mm a') + '</p>'; + '</p>';
+                /* Current conditions */
+                html += '<p class="rn">' + weather.currently + '<p>';
+                /* High & low */
+                html += '<ul class="extremes"><li>' + weather.high + '</li><li>' + weather.low + '</li></ul>'
+                    /* Forecasts */
+                    // Placeholder
+                html += '<div class="row"><div class="col s3"> </div>';
+                for (var i = 1; i < 4; i++) {
+                    html += '<div class="col s2 forecast"><img class="thumb" src="images/icons/' + weather.forecast[i].code + '.svg"><span><p>' + weather.forecast[i].day + '</p><ul class="extremes small"><li>' + weather.forecast[i].high + '</li><li>' + weather.forecast[i].low + '</li></ul></span></div>';
+                }
                 // Placeholder
-            html += '<div class="row"><div class="col s3"> </div>';
-            for (var i = 1; i < 4; i++) {
-                html += '<div class="col s2 forecast"><img class="thumb" src="images/icons/' + weather.forecast[i].code + '.svg"><span><p>' + weather.forecast[i].day + '</p><ul class="extremes small"><li>' + weather.forecast[i].high + '</li><li>' + weather.forecast[i].low + '</li></ul></span></div>';
+                html += '<div class="col s3"></div> </div>';
+
+                $("#roc").html(html);
+            },
+            error: function (error) {
+                $("#roc").html('<p>' + error + '</p>');
             }
-            // Placeholder
-            html += '<div class="col s3"></div> </div>';
+        });
 
-            $("#roc").html(html);
-        },
-        error: function (error) {
-            $("#roc").html('<p>' + error + '</p>');
-        }
-    });
 
+        /* If user has localStorage of locations */
+    } else {
+        var storedLocation1 = localStorage.getItem("weather-location1");
+        var storedLocation2 = localStorage.getItem("weather-location2");
+        
+        $.simpleWeather({
+            location: storedLocation1,
+            woeid: '',
+            unit: 'f',
+            success: function (weather) {
+                /* Weather icon, with alt of current weather */
+                html = '<img class="icon" src="images/icons/' + weather.code + '.svg" alt=' + weather.currently.replace(/\s+/g, '_') + '>';
+                /* Temperature */
+                html += '<h2 class="temp">' + weather.temp + '&deg;</h2>';
+                /* City */
+                html += '<h3 class="city">' + weather.city.toUpperCase() + '</h3>';
+                /* Date & Time */
+                html += '<p class="date">' + moment().format('dddd[,] MMMM Do').toUpperCase();
+                /* Current conditions */
+                html += '<p class="rn">' + weather.currently + '<p>';
+                /* High & low */
+                html += '<ul class="extremes"><li>' + weather.high + '</li><li>' + weather.low + '</li></ul>'
+                    /* Forecasts */
+                    // Placeholder
+                html += '<div class="row"><div class="col s3"> </div>';
+                for (var i = 1; i < 4; i++) {
+                    html += '<div class="col s2 forecast"><img class="thumb" src="images/icons/' + weather.forecast[i].code + '.svg"><span><p>' + weather.forecast[i].day + '</p><ul class="extremes small"><li>' + weather.forecast[i].high + '</li><li>' + weather.forecast[i].low + '</li></ul></span></div>';
+                }
+                // Placeholder
+                html += '<div class="col s3"></div> </div>';
+                $('#la-label').text(weather.city + ', ' + weather.region);
+                $("#la").html(html);
+            },
+            error: function (error) {
+                $("#la").html('<p>' + error + '</p>');
+            }
+        });
+
+
+
+
+        $.simpleWeather({
+            location: storedLocation2,
+            woeid: '',
+            unit: 'f',
+            success: function (weather) {
+                /* Weather icon, with alt of current weather */
+                html = '<img class="icon" src="images/icons/' + weather.code + '.svg" alt=' + weather.currently.replace(/\s+/g, '_') + '>';
+                /* Temperature */
+                html += '<h2 class="temp">' + weather.temp + '&deg;</h2>';
+                /* City */
+                html += '<h3 class="city">' + weather.city.toUpperCase() + '</h3>';
+                /* Date & Time */
+                html += '<p class="date">' + moment().format('dddd[,] MMMM Do').toUpperCase();
+                /* Current conditions */
+                html += '<p class="rn">' + weather.currently + '<p>';
+                /* High & low */
+                html += '<ul class="extremes"><li>' + weather.high + '</li><li>' + weather.low + '</li></ul>'
+                    /* Forecasts */
+                    // Placeholder
+                html += '<div class="row"><div class="col s3"> </div>';
+                for (var i = 1; i < 4; i++) {
+                    html += '<div class="col s2 forecast"><img class="thumb" src="images/icons/' + weather.forecast[i].code + '.svg"><span><p>' + weather.forecast[i].day + '</p><ul class="extremes small"><li>' + weather.forecast[i].high + '</li><li>' + weather.forecast[i].low + '</li></ul></span></div>';
+                }
+                // Placeholder
+                html += '<div class="col s3"></div> </div>';
+                $('#roc-label').text(weather.city + ', ' + weather.region);
+                $("#roc").html(html);
+            },
+            error: function (error) {
+                $("#roc").html('<p>' + error + '</p>');
+            }
+        });
+        
+        $('#custom').after('<br><a id="reset" class="waves-effect waves-dark btn grey darken-3">Reset Saved Locations</a>');
+        $('#reset').click(function() {
+            localStorage.removeItem('weather-location1');
+            localStorage.removeItem('weather-location2');
+            $('#reset').after('<p class="white-text">Saved locations cleared!</p>')
+        });
+    }
+    
+    
     $checkbox.click(function () {
         /* If Rochester */
         if ($checkbox.is(':checked')) {
@@ -102,6 +189,7 @@ $(document).ready(function () {
     });
 
     $('#submit').click(function () {
+        $('#save').slideDown();
         var firstLocation = $('#firstLocation').val();
         var secondLocation = $('#secondLocation').val();
 
@@ -138,10 +226,10 @@ $(document).ready(function () {
                 $("#la").html('<p>' + error + '</p>');
             }
         });
-        
-        
-        
-        
+
+
+
+
         $.simpleWeather({
             location: secondLocation,
             woeid: '',
@@ -175,6 +263,17 @@ $(document).ready(function () {
             }
         });
 
+    });
+
+    $('#save').click(function () {
+        if ($('#firstLocation').val() && $('#secondLocation').val()) {
+            localStorage.setItem('weather-location1', $('#firstLocation').val().toString());
+            localStorage.setItem('weather-location2', $('#secondLocation').val().toString());
+            $('#save').html('Custom locations saved!<br>Visit this page later to see their weather again, automatically!').css('color', 'white');
+
+        } else {
+            alert("Enter two locations in the customization fields to save them locally!");
+        }
     });
 
 });
